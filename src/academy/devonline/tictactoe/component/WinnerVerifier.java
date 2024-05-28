47d3b8ae025fb1521aecd23c16c98f576d276acf
StanKaplan <kaplanst@mail.ru>
2024-05-28 16:40:41 -0400

    Implemented Winne Verifier Component

--- src/academy/devonline/tictactoe/component/WinnerVerifier.java
+++ src/academy/devonline/tictactoe/component/WinnerVerifier.java
@@ -16,6 +16,7 @@
 
 package academy.devonline.tictactoe.component;
 
+import academy.devonline.tictactoe.model.Cell;
 import academy.devonline.tictactoe.model.GameTable;
 
 /**
@@ -23,12 +24,46 @@ import academy.devonline.tictactoe.model.GameTable;
  * link link.com
  */
 public class WinnerVerifier {
-    public boolean isUserWin(GameTable gameTable) {
+    public boolean isUserWin(final GameTable gameTable) {
 
-        return false;
+        return isWinner(gameTable, 'X');
     }
 
-    public boolean isComputerWin(GameTable gameTable) {
+    public boolean isComputerWin(final GameTable gameTable) {
+
+        return isWinner(gameTable, 'O');
+    }
+
+    private boolean isWinner(GameTable gameTable, char sign) {
+        for (int i = 0; i < 3; i++) {
+            int row = 0;
+            for (int j = 0; j < 3; j++) {
+                Cell cell = new Cell(i, j);
+                if (gameTable.getSign(cell) == sign) {
+                    row++;
+                }
+            }
+            if (row == 3) return true;
+        }
+        for (int i = 0; i < 3; i++) {
+            int col = 0;
+            for (int j = 0; j < 3; j++) {
+                Cell cell = new Cell(j, i);
+                if (gameTable.getSign(cell) == sign) {
+                    col++;
+                }
+            }
+            if (col == 3) return true;
+        }
+        int diagonal1 = 0;
+        int diagonal2 = 0;
+        for (int i = 0; i < 3; i++) {
+            Cell cell1 = new Cell(i, i);
+            Cell cell2 = new Cell(2 - i, i);
+            if (gameTable.getSign(cell1) == sign) diagonal1++;
+            if (gameTable.getSign(cell2) == sign) diagonal2++;
+        }
+        if (diagonal1 == 3 || diagonal2 == 3) return true;
 
         return false;
     }
