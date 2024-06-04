d6b3bdf8176ff9f77c0170e86e9f1f8c94c91d44
StanKaplan <kaplanst@mail.ru>
2024-06-04 16:01:16 -0400

    Add TerminalNumericKeypadCellNumberConverter class

--- src/academy/devonline/tictactoe/Launcher.java
+++ src/academy/devonline/tictactoe/Launcher.java
@@ -18,7 +18,7 @@
 package academy.devonline.tictactoe;
 
 import academy.devonline.tictactoe.component.*;
-import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumberConverter;
+import academy.devonline.tictactoe.component.keypad.TerminalNumericKeypadCellNumberConverter;
 
 /**
  * @author devonline
@@ -27,7 +27,7 @@ import academy.devonline.tictactoe.component.keypad.DesktopNumericKeypadCellNumb
 public final class Launcher {
 
     public static void main(final String[] args) {
-        final CellNumberConverter cellNumberConverter = new DesktopNumericKeypadCellNumberConverter();
+        final CellNumberConverter cellNumberConverter = new TerminalNumericKeypadCellNumberConverter();
         final Game game = new Game(
                 new DataPrinter(cellNumberConverter),
                 new ComputerMove(),
